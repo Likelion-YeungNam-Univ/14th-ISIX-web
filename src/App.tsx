cd236a2116d21644845f42ad3f2ac3f7c25afe08
copepb <qwwo5015@gmail.com>
2026-08-05 00:58:20 +0900

    feat: 기본 라우팅 및 페이지 골격 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
+import Upload from '@/pages/Upload';
+import Processing from '@/pages/Processing';
+import Fitting from '@/pages/Fitting';
+import Report from '@/pages/Report';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route path="/processing" element={<Processing />} />
+      <Route path="/fitting" element={<Fitting />} />
+      <Route path="/report" element={<Report />} />
+
+      <Route path="*" element={<NotFound />} />
       {/* TODO
           /upload      사진 입력
           /avatar      아바타 · 치수
@@ -14,6 +25,7 @@ const App = () => {
           /auth/callback  소셜 로그인 콜백
       */}
     </Routes>
+    
   );
 };
 
