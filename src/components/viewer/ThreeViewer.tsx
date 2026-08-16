a58c0023dbac982892f01c503483418f7d2c82c9
knayoung0 <kzeron09@naver.com>
2026-08-16 14:37:54 +0900

    feat : 3D 뷰어 성능 최적화

--- src/components/viewer/ThreeViewer.tsx
+++ src/components/viewer/ThreeViewer.tsx
@@ -27,15 +27,15 @@ const ThreeViewer = ({ avatarUrl = '/models/Duck.glb', garmentUrl ='/models/Dama
     <div className="relative h-full w-full">
       <Suspense fallback={<ViewerLoading className="absolute inset-0"/>}>
         <ViewerErrorBoundary>
-          <Canvas camera={{ position: [0, 1, 3], fov: 45}}>
+          <Canvas camera={{ position: [0, 1, 3], fov: 45}} dpr={[1, 1.5]} gl={{powerPreference: 'high-performance', antialias:true}}>
             <ambientLight intensity={0.6}/>
             <directionalLight position={[3, 5, 2]} intensity={1.2} castShadow/>
             <directionalLight position={[-3, 2, -2]} intensity={0.4}/>
             <Environment preset='studio'/>
             <ContactShadows opacity={0.4} blur={2}/>
             <CameraController/>
-            <AvatarModel key={avatarUrl} url={avatarUrl}/>
-            <GarmentModel key={garmentUrl} ref={garmentMeshRef} url={garmentUrl} preloadUrls={preloadUrls}/>
+            <AvatarModel url={avatarUrl}/>
+            <GarmentModel ref={garmentMeshRef} url={garmentUrl} preloadUrls={preloadUrls}/>
             <HeatmapRenderer meshRef={garmentMeshRef} showHeatmap={showHeatmap} vertexEase={vertexEase} colorScale = {colorScale}/>
             {import.meta.env.DEV && <Stats/>}
           </Canvas>
